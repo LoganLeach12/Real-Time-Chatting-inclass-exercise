ad6f6f597b3ab04a1b492253635f4f8bd1d8c4ed
LoganLeach <122493729+LoganLeach12@users.noreply.github.com>
2023-11-26 19:40:31 -0500

    Update server.js

--- Real-Time-Chatting-main/server.js
+++ Real-Time-Chatting-main/server.js
@@ -7,26 +7,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files
+// Step 8: Serve static files
 app.use(express.static(__dirname));
 
-// Socket.io events
+// Step 9: Handle Socket.io events
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  // Broadcast to all clients when a user joins
+  // Step 10: Broadcast to all clients when a user joins
   socket.broadcast.emit('user joined', 'User');
 
+  // Step 11: Listen for chat messages and broadcast them to all clients
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
 
+  // Step 12: Handle disconnection and broadcast when a user leaves
   socket.on('disconnect', () => {
     console.log('user disconnected');
     io.emit('user left', 'User');
   });
 });
 
+// Step 13: Set up the server to listen on a specific port
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
